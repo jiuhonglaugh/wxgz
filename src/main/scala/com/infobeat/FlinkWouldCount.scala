84dca265dc208f934967c08071bfc5406d02b590
久鸿 <772590001@qq.com>
2019-08-21 17:41:16 +0800

    ai

--- src/main/scala/com/infobeat/FlinkWouldCount.scala
+++ src/main/scala/com/infobeat/FlinkWouldCount.scala
@@ -9,6 +9,7 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 
 object FlinkWouldCount {
+
   private val ZOOKEEPER_HOST = "172.10.4.77:2181,172.10.4.78:2181,172.10.4.79:2181"
   private val KAFKA_BROKER = "172.10.4.78:9092,172.10.4.79:9092,172.10.4.78:9091,172.10.4.79:9091"
   private val TRANSACTION_GROUP = "test_wxgz"
@@ -32,7 +33,7 @@ object FlinkWouldCount {
     val dStream: DataStream[String] = env.addSource(source)
     import org.apache.flink.api.scala._
     val result = dStream.flatMap(x => x.split("\\s"))
-      .map(x => (x, 1)).keyBy(0).timeWindow(Time.seconds(2l)).sum(1)
+      .map(x => (x, 1)).keyBy(0).timeWindow(Time.seconds(2)).sum(1)
 
     result.setParallelism(1).print()
 
