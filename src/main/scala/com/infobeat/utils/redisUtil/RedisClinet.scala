60a476ac398f239af8a41465b702ef0b8067d4bc
久鸿 <34256397+jiuhonglaugh@users.noreply.github.com>
2019-08-24 11:47:14 +0800

    完善redis工具

--- src/main/scala/com/infobeat/utils/redisUtil/RedisClinet.scala
+++ src/main/scala/com/infobeat/utils/redisUtil/RedisClinet.scala
@@ -1,18 +1,14 @@
 package com.infobeat.utils.redisUtil
 
 import java.util
-import java.util.Properties
+
 import com.infobeat.utils.redisUtil.impl.RedisClientImpl
+import org.slf4j.LoggerFactory
 import redis.clients.jedis.Jedis
 
 class RedisClinet extends RedisClientImpl {
 
-  private var pool: BaseRedisConnect = _
-
-  def this(pro: Properties) {
-    this
-    pool = new BaseRedisConnect(pro)
-  }
+  private val LOGGER = LoggerFactory.getLogger(classOf[RedisClinet])
 
   /**
    * 从redis中获取数据
@@ -22,12 +18,12 @@ class RedisClinet extends RedisClientImpl {
     var jedis: Jedis = null
     var key = ""
     try {
-      jedis = pool.getJedis
+      jedis = RedisConnect.getJedis
       key = jedis.get(key)
     } catch {
       case e: Exception =>
-        e.printStackTrace()
-    } finally if (jedis != null) pool.close(jedis)
+        LOGGER.warn(s"获取redis缓存： key = $key 失败", e.printStackTrace())
+    } finally if (jedis != null) RedisConnect.close(jedis)
     key
   }
 
@@ -40,12 +36,12 @@ class RedisClinet extends RedisClientImpl {
     var jedis: Jedis = null
     var keyes: java.util.Set[String] = null
     try {
-      jedis = pool.getJedis
+      jedis = RedisConnect.getJedis
       keyes = jedis.keys(keys)
     } catch {
       case e: Exception =>
-        e.printStackTrace()
-    } finally if (jedis != null) pool.close(jedis)
+        LOGGER.warn(s"获取redis： keys : $keys 失败", e.printStackTrace())
+    } finally if (jedis != null) RedisConnect.close(jedis)
     keyes
   }
 
@@ -59,12 +55,30 @@ class RedisClinet extends RedisClientImpl {
     var jedis: Jedis = null
     var keyes: util.Map[String, String] = null
     try {
-      jedis = pool.getJedis
+      jedis = RedisConnect.getJedis
       keyes = jedis.hgetAll(key)
     } catch {
       case e: Exception =>
-        e.printStackTrace()
-    } finally if (jedis != null) pool.close(jedis)
+        LOGGER.warn(s"获取redis： key : $key 失败", e.printStackTrace())
+    } finally if (jedis != null) RedisConnect.close(jedis)
     keyes
+  }
+
+  /**
+   * 将 Map 数据 写入对应的Key中
+   *
+   * @param key redis key
+   * @param map 即将写入的数据集
+   */
+  override def setMap(key: String, map: util.Map[String, String]): Unit = {
+    var jedis: Jedis = null
+    try {
+      jedis = RedisConnect.getJedis
+      jedis.hmset(key, map)
+      LOGGER.warn(s"redis key : $key 更新成功 数据大小为： ${map.size()}========================")
+    } catch {
+      case e: Exception =>
+        LOGGER.warn(s"redis key : $key 更新失败 数据大小为： ${map.size()}======================", e.printStackTrace())
+    } finally if (jedis != null) RedisConnect.close(jedis)
   }
 }
--- src/main/scala/com/infobeat/utils/redisUtil/impl/RedisClientImpl.scala
+++ src/main/scala/com/infobeat/utils/redisUtil/impl/RedisClientImpl.scala
@@ -4,9 +4,12 @@ import java.util
 
 abstract class RedisClientImpl {
 
+
   def getKey(key: String): String
 
   def getKeys(keys: String): java.util.Set[String]
 
   def getHall(key: String): util.Map[String, String]
+
+  def setMap(key: String, map: util.Map[String, String])
 }
